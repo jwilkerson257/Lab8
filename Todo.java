f2f74ceaaa021b86b5ac47165383fddf8d8ba4a0
Josh <Jwilkerson@ou.edu>
2019-03-28 21:27:21 -0500

    Committing Todo.java

---
+++ Todo.java
@@ -0,0 +1,41 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-03-27
+OU ID_______________: 113406641
+OU EMAIL____________: Jwilkerson@ou.edu
+
+Tasks:
+1. Color
+2. Shape
+3. Location
+4. GamePiece getter methods
+5. GamePiece movesFirst and toString method
+6. GamePieceAppearance
+7. BoardGame addPlayer method
+8. BoardGame getter methods
+9. BoardGame movePlayer and moveTwoPlayers methods
+
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 10
+4. 10
+5. 15
+6. 25
+7. 15
+8. 35
+9. 30
+
+Actual Time (minutes):
+1. 5
+2. 5
+3. 5
+4. 10
+5. 20
+6. 10
+7. 10
+8. 45
+9. 20
+
+*/}
